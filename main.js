5fe2efcd2aa8a50b1c7deea5363bc18abc024892
antonpatotski <patotskianton@gmail.com>
2023-05-18 22:10:21 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css'
+// import init from './EXAMPLE_IMPORTANT.js';
+import { compareExamples, test } from './example.js'
+import { newTest } from "./test.js";
+
+// init();
+compareExamples();
+test();
