c57139ca1e9757537c0f5b1c9d6e0482d23d9629
Lev <pollolev@gmail.com>
2023-05-19 20:05:11 +0300

    hw_2

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 import './style.css'
+import { homeWork }  from "./hw.js";
 // import init from './EXAMPLE_IMPORTANT.js';
-import { compareExamples, test } from './example.js'
-import { newTest } from "./test.js";
+// import { compareExamples, test } from './example.js'
+// import { newTest } from "./test.js";
+
+
 
 // init();
-compareExamples();
-test();
+// compareExamples();
+// test();
+homeWork();
