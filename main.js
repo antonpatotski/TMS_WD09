98900b15b7d223bd27a1e72a47b8a20c9be35f23
vika zhurauliova <vika.zhurauliova.19@gmail.com>
2023-05-20 14:23:52 +0300

    L-2

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ import './style.css'
 import { compareExamples, test } from './example.js'
 import { newTest } from "./test.js";
 
+import { home2 } from './L-2.js'
 // init();
 compareExamples();
 test();
+
+home2();
