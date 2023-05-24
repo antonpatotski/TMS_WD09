f46a8472c2a18bfddec12f914e2c0ec85a604d9a
vika zhurauliova <vika.zhurauliova.19@gmail.com>
2023-05-24 20:12:17 +0300

    L-4

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ import './style.css'
 import { compareExamples, test } from './example.js'
 import { newTest } from "./test.js";
 
-import { home2 } from './L-2.js'
+// import { home2 } from './L-2.js'
+import { home4 } from './L-4.js'
 // init();
 compareExamples();
 test();
 
-home2();
+// home2();
+home4();
