1099cce344e1c667cbb3d2247349385d4ca73d90
antonpatotski <patotskianton@gmail.com>
2023-05-25 23:11:05 +0300

    HW structure

--- HOMEWORK/index.js
+++ HOMEWORK/index.js
@@ -1,8 +1,8 @@
-import { yourFuncName } from "./YOUR_NAME_AND_LAST_NAME/hw_4.js";
+import { yourFuncName as ANTONY_TEST_HW_4 } from "./YOUR_NAME_AND_LAST_NAME/hw_4.js";
 
 
 
 // Не забывайте импортить и экспортить вашу функцию
 export {
-  yourFuncName
+  ANTONY_TEST_HW_4
 }
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as lessonsExamples from "./LESSON/LESSON_4/examples.js";
 import * as hw from "./HOMEWORK";
 
-hw.yourFuncName();
+hw.ANTONY_TEST_HW_4();
 // lessonsExamples.lesson_4();
