5539014e51a21d814ff757ad5676aeace92a614b
vika zhurauliova <vika.zhurauliova.19@gmail.com>
2023-05-26 16:10:34 +0300

    hw_2

--- HOMEWORK/index.js
+++ HOMEWORK/index.js
@@ -1,8 +1,15 @@
 import { yourFuncName as ANTONY_TEST_HW_4 } from "./YOUR_NAME_AND_LAST_NAME/hw_4.js";
 
-
-
+import { home2 as VIKA_ZHURAULIOVA_HW_2 } from "./VIKA_ZHURAULIOVA/hw_2.js";
+import { home4 as VIKA_ZHURAULIOVA_HW_4 } from "./VIKA_ZHURAULIOVA/hw_4.js";
+import { home5 as VIKA_ZHURAULIOVA_HW_5 } from "./VIKA_ZHURAULIOVA/hw_5.js";
 // Не забывайте импортить и экспортить вашу функцию
 export {
   ANTONY_TEST_HW_4
+}
+
+export {
+  VIKA_ZHURAULIOVA_HW_2,
+  VIKA_ZHURAULIOVA_HW_4,
+  VIKA_ZHURAULIOVA_HW_5,
 }
