d5f68dfa2c4bb0b0e33a5b7cd521f0900d8b3e4a
vika zhurauliova <vika.zhurauliova.19@gmail.com>
2023-05-28 12:19:23 +0300

    hw_6

--- HOMEWORK/index.js
+++ HOMEWORK/index.js
@@ -3,6 +3,9 @@ import { yourFuncName as ANTONY_TEST_HW_4 } from "./YOUR_NAME_AND_LAST_NAME/hw_4
 import { home2 as VIKA_ZHURAULIOVA_HW_2 } from "./VIKA_ZHURAULIOVA/hw_2.js";
 import { home4 as VIKA_ZHURAULIOVA_HW_4 } from "./VIKA_ZHURAULIOVA/hw_4.js";
 import { home5 as VIKA_ZHURAULIOVA_HW_5 } from "./VIKA_ZHURAULIOVA/hw_5.js";
+import { home6 as VIKA_ZHURAULIOVA_HW_6 } from "./VIKA_ZHURAULIOVA/hw_6.js";
+import { home7 as VIKA_ZHURAULIOVA_HW_7 } from "./VIKA_ZHURAULIOVA/hw_7.js";
+
 // Не забывайте импортить и экспортить вашу функцию
 export {
   ANTONY_TEST_HW_4
@@ -12,4 +15,6 @@ export {
   VIKA_ZHURAULIOVA_HW_2,
   VIKA_ZHURAULIOVA_HW_4,
   VIKA_ZHURAULIOVA_HW_5,
+  VIKA_ZHURAULIOVA_HW_6,
+  VIKA_ZHURAULIOVA_HW_7,
 }
--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 import * as lessonsExamples from "./LESSON/LESSON_4/examples.js";
 import * as hw from "./HOMEWORK";
-import {VIKA_ZHURAULIOVA_HW_2} from "./HOMEWORK";
-import {VIKA_ZHURAULIOVA_HW_4} from "./HOMEWORK";
-import {VIKA_ZHURAULIOVA_HW_5} from "./HOMEWORK";
-
 
 // hw.ANTONY_TEST_HW_4();
 // // lessonsExamples.lesson_4();
-// VIKA_ZHURAULIOVA_HW_2();
-hw.VIKA_ZHURAULIOVA_HW_4();
+// hw.VIKA_ZHURAULIOVA_HW_2();
+// hw.VIKA_ZHURAULIOVA_HW_4();
 // hw.VIKA_ZHURAULIOVA_HW_5();
+hw.VIKA_ZHURAULIOVA_HW_6();
+// hw.VIKA_ZHURAULIOVA_HW_7();
