313b768a395dfe50342de5274af571e3b3888333
antonpatotski <patotskianton@gmail.com>
2023-05-28 18:02:36 +0300

    HW structure

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
+// DO NOT COMMIT ON THIS FILE. CHANGES ONLY IN YOUR BRANCH. THEN DELETE
 import * as lessonsExamples from "./LESSON/LESSON_4/examples.js";
 import * as HOMEWORK from "./HOMEWORK";
 
-// console.log(HOMEWORK.ROMAN_SEMENYK_HW.ROMAN_SEMENYK_HW_5()); // EXAMPLE
+// console.log(HOMEWORK.ROMAN_SEMENYK_HW.ROMAN_SEMENYK_HW_5()); // EXAMPLE. No any chages on file. Only in your personal branch.
