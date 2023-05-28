46f62ea1ccd79eb4fe89045d4ca8b6de1af8f8c6
antonpatotski <patotskianton@gmail.com>
2023-05-28 18:01:22 +0300

    HW structure

--- HOMEWORK/index.js
+++ HOMEWORK/index.js
@@ -1,9 +1,7 @@
+import * as ROMAN_SEMENYK_HW from './RomanSemenyk';
+import * as TEST_TEMPLATE from './YOUR_NAME_AND_LAST_NAME'
 
-import {home4 as ROMAN_SEMENYK_HW_4} from "./RomanSemenyk/hw_4.js";
-import {home5 as ROMAN_SEMENYK_HW_5} from "./RomanSemenyk/hw_5.js";
-
-// Не забывайте импортить и экспортить вашу функцию
 export {
-  ROMAN_SEMENYK_HW_4,
-  ROMAN_SEMENYK_HW_5
+  ROMAN_SEMENYK_HW,
+  TEST_TEMPLATE
 }
--- main.js
+++ main.js
@@ -1,8 +1,4 @@
 import * as lessonsExamples from "./LESSON/LESSON_4/examples.js";
-import * as hw from "./HOMEWORK";
-import {ROMAN_SEMENYK_HW_4} from "./HOMEWORK";
-import {ROMAN_SEMENYK_HW_5} from "./HOMEWORK";
+import * as HOMEWORK from "./HOMEWORK";
 
-
-hw.ROMAN_SEMENYK_HW_4();
-hw.ROMAN_SEMENYK_HW_5();
+// console.log(HOMEWORK.ROMAN_SEMENYK_HW.ROMAN_SEMENYK_HW_5()); // EXAMPLE
