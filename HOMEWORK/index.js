a8226d7d687f8a9f64261817c4898aef845ff0f8
antonpatotski <patotskianton@gmail.com>
2023-06-01 21:33:07 +0300

    resolve conflicts

--- HOMEWORK/index.js
+++ HOMEWORK/index.js
@@ -1,22 +1,12 @@
 import { yourFuncName as ANTONY_TEST_HW_4 } from "./YOUR_NAME_AND_LAST_NAME/hw_4.js";
 
-import { home2 as VIKA_ZHURAULIOVA_HW_2 } from "./VIKA_ZHURAULIOVA/hw_2.js";
-import { home4 as VIKA_ZHURAULIOVA_HW_4 } from "./VIKA_ZHURAULIOVA/hw_4.js";
-import { home5 as VIKA_ZHURAULIOVA_HW_5 } from "./VIKA_ZHURAULIOVA/hw_5.js";
-import { home6 as VIKA_ZHURAULIOVA_HW_6 } from "./VIKA_ZHURAULIOVA/hw_6.js";
-import { home7 as VIKA_ZHURAULIOVA_HW_7 } from "./VIKA_ZHURAULIOVA/hw_7.js";
-import { addition as VIKA_ZHURAULIOVA_ADDITION } from "./VIKA_ZHURAULIOVA/addition.js";
 import * as ROMAN_SEMENYK_HW from './RomanSemenyk';
-import * as TEST_TEMPLATE from './YOUR_NAME_AND_LAST_NAME'
+import * as TEST_TEMPLATE from './YOUR_NAME_AND_LAST_NAME';
+import * as VIKA_ZHURAULIOVA_HW from './VIKA_ZHURAULIOVA'
 
 // Не забывайте импортить и экспортить вашу функцию
 export {
-  VIKA_ZHURAULIOVA_HW_2,
-  VIKA_ZHURAULIOVA_HW_4,
-  VIKA_ZHURAULIOVA_HW_5,
-  VIKA_ZHURAULIOVA_HW_6,
-  VIKA_ZHURAULIOVA_HW_7,
-  VIKA_ZHURAULIOVA_ADDITION,
+  VIKA_ZHURAULIOVA_HW,
   ROMAN_SEMENYK_HW,
   TEST_TEMPLATE,
 }
