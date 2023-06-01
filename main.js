8205347e34d0cb9e06e103ce98027c1c9ae220ad
antonpatotski <patotskianton@gmail.com>
2023-06-01 21:22:19 +0300

    LESSON_6

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 // DO NOT COMMIT ON THIS FILE. CHANGES ONLY IN YOUR BRANCH. THEN DELETE
-import * as lessonsExamples from "./LESSON/LESSON_4/examples.js";
+import * as bootstrap from 'bootstrap';
+import * as lessonsExamples from "./LESSON";
 import * as HOMEWORK from "./HOMEWORK";
+import './style.scss';
 
 // console.log(HOMEWORK.ROMAN_SEMENYK_HW.ROMAN_SEMENYK_HW_5()); // EXAMPLE. No any chages on file. Only in your personal branch.
+
+HOMEWORK.TEST_TEMPLATE.yourFuncName()
+
+lessonsExamples.lesson_6();
+
+// console.log(bootstrap)
