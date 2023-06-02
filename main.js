7984ce73531c0b0f0098fc0d3731af339bfb07b3
antonpatotski <patotskianton@gmail.com>
2023-06-02 19:52:54 +0300

    Fix conflicts

--- main.js
+++ main.js
@@ -4,21 +4,5 @@ import * as lessonsExamples from "./LESSON";
 import * as HOMEWORK from "./HOMEWORK";
 import './style.scss';
 
-<<<<<<<<< Temporary merge branch 1
-// hw.ANTONY_TEST_HW_4();
-// // lessonsExamples.lesson_4();
-// hw.VIKA_ZHURAULIOVA_HW_2();
-// hw.VIKA_ZHURAULIOVA_HW_4();
-// hw.VIKA_ZHURAULIOVA_HW_5();
-// hw.VIKA_ZHURAULIOVA_HW_6();
-// hw.VIKA_ZHURAULIOVA_HW_7();
-hw.VIKA_ZHURAULIOVA_ADDITION();
-=========
-// console.log(HOMEWORK.ROMAN_SEMENYK_HW.ROMAN_SEMENYK_HW_5()); // EXAMPLE. No any chages on file. Only in your personal branch.
 
-HOMEWORK.TEST_TEMPLATE.yourFuncName()
-
-// lessonsExamples.lesson_6();
-
-// console.log(bootstrap)
->>>>>>>>> Temporary merge branch 2
+HOMEWORK.VIKA_ZHURAULIOVA_HW.VIKA_ZHURAULIOVA_HW_8();
