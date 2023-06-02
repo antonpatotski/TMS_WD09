7d8d0761afba79b15b70ef92bc4751579d480922
xxsamgelik <xxsamgelik@mail.ru>
2023-06-02 15:18:21 +0300

    hw4 && hw5

--- HOMEWORK/index.js
+++ HOMEWORK/index.js
@@ -1,9 +1,12 @@
 
 import {home4 as ROMAN_SEMENYK_HW_4} from "./RomanSemenyk/hw_4.js";
 import {home5 as ROMAN_SEMENYK_HW_5} from "./RomanSemenyk/hw_5.js";
+import {home6 as ROMAN_SEMENYK_HW_6} from "./RomanSemenyk/hw_6.js";
 
 // Не забывайте импортить и экспортить вашу функцию
 export {
   ROMAN_SEMENYK_HW_4,
-  ROMAN_SEMENYK_HW_5
+  ROMAN_SEMENYK_HW_5,
+  ROMAN_SEMENYK_HW_6,
+
 }
--- main.js
+++ main.js
@@ -2,7 +2,8 @@ import * as lessonsExamples from "./LESSON/LESSON_4/examples.js";
 import * as hw from "./HOMEWORK";
 import {ROMAN_SEMENYK_HW_4} from "./HOMEWORK";
 import {ROMAN_SEMENYK_HW_5} from "./HOMEWORK";
+import {ROMAN_SEMENYK_HW_6} from "./HOMEWORK";
 
 
-hw.ROMAN_SEMENYK_HW_4();
-hw.ROMAN_SEMENYK_HW_5();
+
+hw.ROMAN_SEMENYK_HW_6();
