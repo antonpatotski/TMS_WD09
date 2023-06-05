bef8e0bde21a5145cdf5c59786b9f9f202441a14
Aliaksandr Zakharevich <jonnydukalez@gmail.com>
2023-06-05 13:09:30 +0300

    add hw_6

--- HOMEWORK/AliaksandrZakharevich/index.js
+++ HOMEWORK/AliaksandrZakharevich/index.js
@@ -2,9 +2,9 @@ import { homework_2 as ALIAKSANDR_ZAKHAREVICH_HW_2 } from "./hw_2.js";
 import { homework_3 as ALIAKSANDR_ZAKHAREVICH_HW_3 } from "./hw_3.js";
 import { homework_4 as ALIAKSANDR_ZAKHAREVICH_HW_4 } from "./hw_4.js";
 import { homework_5 as ALIAKSANDR_ZAKHAREVICH_HW_5 } from "./hw_5.js";
+import { homework_6 as ALIAKSANDR_ZAKHAREVICH_HW_6 } from "./hw_6.js";
 
 // Не забывайте импортить и экспортить вашу функцию
 export {
-    ALIAKSANDR_ZAKHAREVICH_HW_4,
-    ALIAKSANDR_ZAKHAREVICH_HW_5
+    ALIAKSANDR_ZAKHAREVICH_HW_6
 }
