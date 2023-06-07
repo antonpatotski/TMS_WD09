42a0c1bc1a24604bb85194494a17b4b0756d458a
vika zhurauliova <vika.zhurauliova.19@gmail.com>
2023-06-07 22:07:31 +0300

    hw_10

--- HOMEWORK/index.js
+++ HOMEWORK/index.js
@@ -1,10 +1,9 @@
 
-
+import * as  ROMAN_SEMENYK_HW from "./RomanSemenyk"
 import * as VIKA_ZHURAULIOVA_HW from "./VIKA_ZHURAULIOVA";
 
 // Не забывайте импортить и экспортить вашу функцию
 export {
   ROMAN_SEMENYK_HW,
-  TEST_TEMPLATE,
   VIKA_ZHURAULIOVA_HW
 }
