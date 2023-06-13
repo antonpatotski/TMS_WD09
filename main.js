d0466029259e151e5c5e3a524f69f0fe69d2b813
antonpatotski <patotskianton@gmail.com>
2023-06-13 14:13:03 +0300

    // BOOTSTRAP_IMPORT_LINE

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 // DO NOT COMMIT ON THIS FILE. CHANGES ONLY IN YOUR BRANCH. THEN DELETE
-import * as bootstrap from 'bootstrap';
+import 'bootstrap';
+// BOOTSTRAP_IMPORT_LINE
 import * as lessonsExamples from "./LESSON";
 import * as HOMEWORK from "./HOMEWORK";
 import './style.scss';
 
-// lessonsExamples.lesson_8();
+// lessonsExamples.lesson_7();
